bd6aac6c99ee80892b8d060c4cf5bcb18933edb5
Joish <1270685166@qq.com>
2021-02-21 14:51:14 +0800

    deploy

---
+++ precache-manifest.536ec977efed43aa2ff3a9d02e4d27cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfac94ec9384968cf2b",
+    "url": "/book-keeping-website/css/app.ae8b4f05.css"
+  },
+  {
+    "revision": "5aa737c4e25c14a806dd87adf245209b",
+    "url": "/book-keeping-website/index.html"
+  },
+  {
+    "revision": "6dfac94ec9384968cf2b",
+    "url": "/book-keeping-website/js/app.8c256ae9.js"
+  },
+  {
+    "revision": "517f69bc21442a474330",
+    "url": "/book-keeping-website/js/chunk-vendors.1f05eb32.js"
+  },
+  {
+    "revision": "9687f909ab6db5f1b756e536556c3b17",
+    "url": "/book-keeping-website/manifest.json"
+  },
+  {
+    "revision": "1422364220db4462a0f386a601ac3db0",
+    "url": "/book-keeping-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/book-keeping-website/robots.txt"
+  }
+]);
